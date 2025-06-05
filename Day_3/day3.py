0d4c6fce68dd074be382ebd1d198d77cf69cd0e7
UP240408 <up240408@alumnos.upa.edu.mx>
2025-06-05 11:38:15 -0600

    Hola causas

---
+++ Day_3/day3.py
@@ -0,0 +1,37 @@
+age = int
+height = float
+that_store = complex
+
+#Area de un triangulo
+print("Calculo para el area de un triangulo")
+print("Ingresa la base del triangulo")
+Base = int(input(""))
+print("Ingresa la altura del triangulo")
+Altura = int(input(""))
+Area = (Base*Altura)/2
+print("El area del triangulo es = ",Area)
+
+#Perimetro de un triangulo
+print("Calcular el perimetro de un triangulo")
+Lado_a = int(input("Ingresa el lado a "))
+Lado_b = int(input("Ingresa el lado b "))
+Lado_c = int(input("Ingresa el lado c "))
+Perimetro = Lado_a+Lado_b+Lado_c
+print("El Perimetro del triangulo es = ",Perimetro)
+
+#Area de un rectangulo
+
+print("Calculo para el area de un rectangulo")
+print("Ingresa la base del rectangulo")
+Base = int(input(""))
+print("Ingresa la altura del rectangulo")
+Altura = int(input(""))
+Area = (Base*Altura)
+print("El area del rectangulo es = ",Area)
+
+#Perimetro de un rectangulo
+print("Calcular el perimetro de un triangulo")
+Lado_a = int(input("Ingresa el lado a "))
+Lado_b = int(input("Ingresa el lado b "))
+Perimetro = 2*(Lado_a+Lado_b)
+print("El Perimetro del triangulo es = ",Perimetro)
