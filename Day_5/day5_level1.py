23cb89cf6b43c3642cd83fc8446dff74a1ebe835
UP240408 <up240408@alumnos.upa.edu.mx>
2025-06-26 08:55:46 -0600

    f

--- Day_5/day5_level1.py
+++ Day_5/day5_level1.py
@@ -1,11 +1,18 @@
 #1
 lst = list()
-print(len(lst))
-
 #2
+lst2 = ['Pino','Arce','Arbol','Alto','Largo','Platanero','Cactus']
 #3
+print(len(lst2))
 #4
+first_item = lst2[-7]
+print(first_item)
+middle_item = lst2[-4]
+print(middle_item)
+final_item = lst2[-1]
+print(final_item)
 #5
+mixed_data_types = ['name', 'age', 'height', 'marital_status', 'address']
 #6
 #7
 #8
